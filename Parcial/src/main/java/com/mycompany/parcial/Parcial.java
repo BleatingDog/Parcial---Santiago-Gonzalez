722e301277891b727e825895052f35ea737df08d
Santiago González <santiago.galvez@correounivalle.edu.co>
2023-01-19 10:19:19 -0500

    Primer commit

---
+++ Parcial/src/main/java/com/mycompany/parcial/Parcial.java
@@ -0,0 +1,14 @@
+/*
+* Parcial programación
+* Santiago González Gálvez - 202183392
+* Profesor: Luis Yovany Romo Portilla
+*/
+
+package com.mycompany.parcial;
+
+public class Parcial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
